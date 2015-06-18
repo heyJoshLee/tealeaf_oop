47192078d070898cba37873d611cc7d4bc485139
Josh Lee <Lee45276@gmail.com>
2015-06-18 10:43:32 +0800

    Initial commit

---
+++ oop_workbook/easy.rb
@@ -0,0 +1,20 @@
+class Greeting
+  def greet(string)
+    puts string
+  end
+end
+
+class Hello < Greeting
+  def hi
+    greet("Hello")
+  end
+end
+
+class Goodbye < Greeting
+  def bye
+    greet("Goodbye")
+  end
+end
+
+saying = Hello.new
+saying.hi
